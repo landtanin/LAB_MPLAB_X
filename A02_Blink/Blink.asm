061b5111c017260b1572e3d6e9e7d64377bbd2bd
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-11-02 21:49:00 +0000

    Finish task 2, left with one question about Delay1

--- A02_Blink/Blink.asm
+++ A02_Blink/Blink.asm
@@ -12,9 +12,12 @@
 #include "prologue.inc"
 
 ; declaration of register variables (unsigned char)
-     cblock 0x20
+     cblock 0x20     ; since the first 32 bits of data memory is for SFR, to allocate bit for memory it need to start from 32 (0x20)
 Delay1               ; these variables are used as counters to provide delays
-Delay2               
+Delay2
+     
+; try for fun
+;Delay3
      endc
 
 
@@ -31,8 +34,14 @@ Start:
 ;* superloop
 MainLoop:
      bsf       PORTD,RD0        ; turn on LED RD0
-     movlw     0xff
-     movwf     Delay2
+     
+     ; try for fun
+;     movlw     0xff
+;     movwf     Delay1
+     
+;      exercise 1
+     movlw     0x7f
+     movwf     Delay2  ; why don't assign to Delay1??????????????
 
 ; delay for displaying the LED ON
 OndelayLoop:
@@ -41,6 +50,10 @@ OndelayLoop:
      decfsz    Delay2,f       ; The outer loop takes and additional 3 instructions per lap * 256 loops
      goto      OndelayLoop    ; (768+3) * 256 = 197376 instructions / 1M instructions per second = 0.197 sec.
                               ; call it two-tenths of a second.
+			
+     ; try for fun
+;     decfsz    Delay3, f
+;     goto      OndelayLoop    ; (197376+3) * 256 = 50,529,024 instructions / 1M instructions per second ~ 50 sec
      
      bcf       PORTD,RD0      ; Turn off LED RD0
 
